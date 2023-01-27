6bf3da85a9785b14fb2e62059ae2a54ed824058c
sarthakbag30 <58979606+sarthakbag30@users.noreply.github.com>
2023-01-27 10:03:14 +0530

    utils updated to get posted data

--- BackEnd/utils/utils.js
+++ BackEnd/utils/utils.js
@@ -1,1 +1,21 @@
+function getPostData(req) {
+    return new Promise((resolve, reject) => {
+        try {
+            let body = ''
 
+            req.on('data', (chunk) => {
+                body += chunk
+            })
+            req.on('end', () => {
+                resolve(body)
+            })
+            
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+module.exports = {
+    getPostData
+}
