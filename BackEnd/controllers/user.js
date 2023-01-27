f68abe18b7230fc5c7f20dfd6ccbe51e405e4de7
sarthakbag30 <58979606+sarthakbag30@users.noreply.github.com>
2023-01-27 10:07:28 +0530

    Query file initial commit

---
+++ BackEnd/controllers/user.js
@@ -0,0 +1,35 @@
+// import { MongoClient } from "mongodb";
+
+// const dbURI = 'mongodb+srv://test:1234@cluster0.xqjdndp.mongodb.net/?retryWrites=true&w=majority';
+// const database = client.db('soProject');
+
+
+// checkUser = (req,res) =>{
+//     try{
+
+//     }catch(err){
+//         console.log('Check user failed');
+//         res.end();
+//     }
+// }
+
+userSignin = (req,res) =>{
+    try{
+        
+        console.log('User signed in');
+    }catch(err){
+        console.log('Error in user sign in');
+    }
+}
+
+userSignup = (req,res) =>{
+    try{
+
+
+        console.log('User signed up');
+    }catch(err){
+        console.log('Error in user sign up');
+    }
+
+}
+
