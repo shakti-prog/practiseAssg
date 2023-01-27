bf236c87378e3bfc8fe1ee154dde55a7c14989a2
sarthakbag30 <58979606+sarthakbag30@users.noreply.github.com>
2023-01-27 16:40:06 +0530

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const databaseName = "soProject";
-let mongoutil = require( './utils/mongoutil' );
+var mongoutil = require( './utils/mongoutil' );
 
 const{
     userSignin,
@@ -17,20 +16,8 @@ const{
 
 mongoutil.connectToServer( function( err, client ) {
     if (err) console.log(err);
-  } );
-
-
-
-// MongoClient.connect(dburi, { useNewUrlParser: true }, async (error, client) => {
-//   if (error) {
-//     return console.log("Connection failed for some reason");
-//   }
-//   console.log("Connection established - All well");
-//   const db = client.db(databaseName);
-//   const result = await db.collection('ticket').find({}).toArray();
-//   console.log(result)
-// });
-
+    console.log('db connnected')
+});
 
 const server = http.createServer((req, res) => {
 
@@ -42,42 +29,16 @@ const server = http.createServer((req, res) => {
         showQueries(req,res);
     }else if(req.url === '/createquery' && req.method === 'POST'){
         addQuery(req,res)
-    }else if(req.url === '/' && req.method === 'GET'){
+    }else if(req.url === '/resolvequery' && req.method === 'POST'){
         updateQuery(req,res);
-    }else if(req.url === '/' && req.method === 'GET'){
+    }else if(req.url === '/deletequery' && req.method === 'POST'){
         deleteQuery(req,res);
     }else{
         res.writeHead(404,{'Content-Type':'application/json'})
         res.end(JSON.stringify({message:'Error 404, Page not found'}))
     }
-    /*switch(req.url){
-        case '/':
-            console.log("SignIn")
-            userSignin(req,res);
-            break;
-        case '/signup':
-            console.log("Signup");
-            userSignup(req,res);
-            break;
-        case '/allqueries':
-            console.log("All queries");
-            showQueries(req,res);
-        case '/createquery':
-            console.log("Create Query");
-            addQuery(req,res);
-            break;
-        case '/resolvequery':
-            console.log("Resolve Query");
-            updateQuery(req,res);
-            break;
-        case '/deletequery':
-            console.log("Delete query");
-            deleteQuery(req,res);
-        default:
-            console.log("404");
-            res.statusCode = 404;
-            break;
-    }*/
+
+       
 });
 
 server.listen(3000,'localhost',()=>{
