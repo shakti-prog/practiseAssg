35735fc66ece49b160cbe0944eee94615d8fdc0c
sarthakbag30 <58979606+sarthakbag30@users.noreply.github.com>
2023-01-27 12:59:38 +0530

    updated app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,48 +1,90 @@
 const http = require('http');
 const fs = require('fs');
+const databaseName = "soProject";
+let mongoutil = require( './utils/mongoutil' );
+
+const{
+    userSignin,
+    userSignup
+} = require('./controllers/user');
+
+const{
+    showQueries,
+    addQuery,
+    updateQuery,
+    deleteQuery
+} = require('./controllers/query')
+
+mongoutil.connectToServer( function( err, client ) {
+    if (err) console.log(err);
+  } );
+
+
+
+// MongoClient.connect(dburi, { useNewUrlParser: true }, async (error, client) => {
+//   if (error) {
+//     return console.log("Connection failed for some reason");
+//   }
+//   console.log("Connection established - All well");
+//   const db = client.db(databaseName);
+//   const result = await db.collection('ticket').find({}).toArray();
+//   console.log(result)
+// });
 
 
 const server = http.createServer((req, res) => {
-    //console.log(req.url , req.method);
 
-    res.setHeader('Content-Type','text/html');
+    if(req.url === '/' && req.method === 'GET'){
+        //userSignin(req,res);
+    }else if(req.url === '/' && req.method === 'GET'){
 
-    let path = './views/';
-
-    switch(req.url){
+    }else if(req.url === '/allqueries' && req.method === 'GET'){
+        showQueries(req,res);
+    }else if(req.url === '/createquery' && req.method === 'POST'){
+        addQuery(req,res)
+    }else if(req.url === '/' && req.method === 'GET'){
+        updateQuery(req,res);
+    }else if(req.url === '/' && req.method === 'GET'){
+        deleteQuery(req,res);
+    }else{
+        res.writeHead(404,{'Content-Type':'application/json'})
+        res.end(JSON.stringify({message:'Error 404, Page not found'}))
+    }
+    /*switch(req.url){
         case '/':
-            path+= 'login.html';
-            res.statusCode = 200;
+            console.log("SignIn")
+            userSignin(req,res);
             break;
-        case '/homepage':
-            path+= 'homepage.html';
-            res.statusCode = 200;
+        case '/signup':
+            console.log("Signup");
+            userSignup(req,res);
             break;
-        case '/postquery':
-            path+= 'postquery.html';
-            res.statusCode = 200;
+        case '/allqueries':
+            console.log("All queries");
+            showQueries(req,res);
+        case '/createquery':
+            console.log("Create Query");
+            addQuery(req,res);
             break;
         case '/resolvequery':
-            path+= 'resolvequery.html';
-            res.statusCode = 200;
+            console.log("Resolve Query");
+            updateQuery(req,res);
             break;
+        case '/deletequery':
+            console.log("Delete query");
+            deleteQuery(req,res);
         default:
-            path+= '404.html';
+            console.log("404");
             res.statusCode = 404;
             break;
-    }
-    
-    fs.readFile(path,(err,data)=>{
-        if(err){
-            console.log(err);
-            res.end();
-        }else{
-            res.write(data);
-            res.end();
-        }
-    });
+    }*/
 });
 
 server.listen(3000,'localhost',()=>{
     console.log('Listening for requests on port 3000')
 });
+
+
+ module.exports = {
+
+}
